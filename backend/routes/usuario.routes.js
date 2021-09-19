bf49f7c97ad90e4bce77f1cd2d971063c70f0d9b
svasquez <santivasquez1@gmail.com>
2021-09-19 18:05:05 -0500

    Encriptacion de password de los usuarios

--- backend/routes/usuario.routes.js
+++ backend/routes/usuario.routes.js
@@ -10,7 +10,7 @@ const { validarCampos } = require('../middlewares/validarCampos');
 const router = Router();
 router.get('/', getUsuarios);
 router.post('/', [
-        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('password', 'El password es obligatorio').not().isEmpty(),
         check('email', 'El email no tiene el formato correcto').isEmail(),
         validarCampos
