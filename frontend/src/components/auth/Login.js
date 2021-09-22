356ea4b9fa8f75c4061721030c9c9be91f58c14b
eliablm <eliablopez@hotmail.com>
2021-09-21 20:51:24 -0500

    Agrega estructura Login

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,9 +1,38 @@
 import React, { Fragment } from 'react';
 
 function Login() {
+	const onChange = () => {
+		console.log('Escribiendo...');
+	};
+
 	return (
 		<Fragment>
-			<h1>Desde login</h1>
+			<form>
+				<h1>Iniciar Sesión</h1>
+				<div>
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						placeholder="Email"
+						onChange={onChange}
+					></input>
+				</div>
+				<div>
+					<label htmlFor="password">Contraseña</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						placeholder="Contraseña"
+						onChange={onChange}
+					></input>
+				</div>
+				<div>
+					<input type="submit" value="Iniciar Sesión" />
+				</div>
+			</form>
 		</Fragment>
 	);
 }
