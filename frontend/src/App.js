b5077b0df8b254db4e65094195f6d9f25ad214b8
eliablm <eliablopez@hotmail.com>
2021-09-23 12:56:43 -0500

    Agrega estilo neutro al Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Login } from './components/auth/Login';
 import { NuevaCuenta } from './components/auth/NuevaCuenta';
+import { Dashboard } from './components/layout/Dashboard';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={Login} />
 				<Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+				<Route exact path="/dashboard" component={Dashboard} />
 			</Switch>
 		</Router>
 	);
--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import './Login.css';
 
 function Login() {
 	const onChange = () => {
@@ -7,32 +8,34 @@ function Login() {
 
 	return (
 		<Fragment>
-			<form>
-				<h1>Iniciar Sesión</h1>
-				<div>
-					<label htmlFor="email">Email</label>
-					<input
-						type="email"
-						id="email"
-						name="email"
-						placeholder="Email"
-						onChange={onChange}
-					></input>
-				</div>
-				<div>
-					<label htmlFor="password">Contraseña</label>
-					<input
-						type="password"
-						id="password"
-						name="password"
-						placeholder="Contraseña"
-						onChange={onChange}
-					></input>
-				</div>
-				<div>
-					<input type="submit" value="Iniciar Sesión" />
-				</div>
-			</form>
+			<div className="login">
+				<form>
+					<h1>Iniciar Sesión</h1>
+					<div className="email field">
+						<label htmlFor="email">Email</label>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							placeholder="Email"
+							onChange={onChange}
+						></input>
+					</div>
+					<div className="password field">
+						<label htmlFor="password">Contraseña</label>
+						<input
+							type="password"
+							id="password"
+							name="password"
+							placeholder="Contraseña"
+							onChange={onChange}
+						></input>
+					</div>
+					<div className="submit-btn">
+						<input type="submit" value="Iniciar Sesión" />
+					</div>
+				</form>
+			</div>
 		</Fragment>
 	);
 }
