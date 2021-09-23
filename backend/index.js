3d38fe6d9c710145f4c81db69437347d096c3b0c
svasquez <santivasquez1@gmail.com>
2021-09-23 07:16:41 -0500

    Se agrega swagger al proyecto, para la documentacion de la api

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const { bdConnection } = require('./database/config');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 //Servidor
 const app = express();
 
@@ -17,11 +19,12 @@ app.use(express.json());
 bdConnection();
 
 //Carpeta con la info de la apirest
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 //Rutas
 app.use('/api/usuarios', require('./routes/usuario.routes'));
 app.use('/api/login', require('./routes/auth.route'));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT, () => {
     console.log("Servidor corriendo en el puerto " + process.env.PORT);
